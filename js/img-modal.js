9a5462a85031d89a90894df1d5ac4ac5cd4b53e5
Oleg Troian <sleite4r@gmail.com>
2024-10-26 17:30:33 +0200

    create img object

---
+++ js/img-modal.js
@@ -0,0 +1,17 @@
+const gallery = document.querySelector(".gallery-wrapper");
+
+let img = [
+  "../imgages/halloween-4.jpg",
+  "../imgages/halloween-5.jpg",
+  "../imgages/halloween-6.jpg",
+  "../imgages/halloween-7.jpg",
+  "../imgages/halloween-8.jpg",
+  "../imgages/halloween-9.jpg",
+  "../imgages/halloween-10.jpg",
+  "../imgages/halloween-11.jpg",
+  "../imgages/halloween-12.jpg"
+];
+
+img.forEach(element => {
+  gallery.innerHTML += `<img src='${element}' class='img'>`;
+});
--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,19 @@
+
+// MODAL-ABOUT
+
 (() => {
-  const refs = {
+  const modal = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  modal.openModalBtn.addEventListener("click", toggleModal);
+  modal.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
+    modal.modal.classList.toggle("is-hidden");
   }
 })();
+
+
