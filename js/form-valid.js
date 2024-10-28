7b037dadec26471f692e1a4559aa98de045fa5c7
Oleg Troian <sleite4r@gmail.com>
2024-10-28 18:29:13 +0100

    hero-text{font-size 18x }

--- js/form-valid.js
+++ js/form-valid.js
@@ -4,24 +4,31 @@ document.querySelector("#login-form").addEventListener("submit", function (event
   const form = event.currentTarget;
   const user = form.elements.name.value.trim();
   const email = form.elements.email.value.trim();
-  const message = document.querySelector('.mesg');
+
+  //! check method trim() if work !
+  // console.log("User input before trim:", form.elements.name.value);
+  // console.log("User input after trim:", user);
+  // console.log("Email input before trim:", form.elements.email.value);
+  // console.log("Email input after trim:", email);
+
+  const message = document.querySelector(".mesg");
   const heading = document.querySelector(".contact-title");
 
-  if (user === "" || email === '') {
+  if (user === "" || email === "") {
     alert("All form fields must be filled in");
     return;
   }
 
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailPattern.test(email)) { 
-    // console.log("not valid email"); 
+  if (!emailPattern.test(email)) {
+    // console.log("not valid email");
     message.innerHTML = "not valid email";
     return;
   }
   // console.log("valid email");
-  
-  message.innerHTML = '';
-  form.classList.add('none');
+
+  message.innerHTML = "";
+  form.classList.add("none");
   heading.innerHTML = "we will contact you";
 
   const formData = {
@@ -31,5 +38,4 @@ document.querySelector("#login-form").addEventListener("submit", function (event
 
   console.log(formData);
   form.reset();
-
 });
